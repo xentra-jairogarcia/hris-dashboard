d9c8c326b07e6f51eeb191d0495d20a11bfefae0
xentra-jairogarcia <jairo.garcia.xentra@gmail.com>
2026-03-13 03:42:43 +0800

    update scss main-layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainLayout } from './core/layouts/main-layout/main-layout';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Employees } from './pages/employees/employees';
+import { Projects } from './pages/projects/projects';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,7 @@ export const routes: Routes = [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: Dashboard },
       { path: 'employees', component: Employees },
+      { path: 'projects', component: Projects },
     ]
   }
 ];
